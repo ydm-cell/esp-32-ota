5de0c045c6eabebf7c8f98d8880c7bbf25c45510
ydm-cell <sec23ci006@sairamtap.edu.in>
2026-06-10 14:00:15 +0530

    Add files via upload

---
+++ sketch_sep13a.ino
@@ -0,0 +1,13 @@
+#define LED_PIN 2
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH); // LED ON
+  delay(1000);
+
+  digitalWrite(LED_PIN, LOW);  // LED OFF
+  delay(1000);
+}
